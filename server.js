544ceeb879f4880c447a217bbaee041f71e4a13f
wealthiduwe <wealthiduwe@gmail.com>
2023-06-05 16:10:39 -0400

    Add: Added factory function to create endpoints easily

---
+++ server.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! ✴ Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successfully'));
+
+const PORT = process.env.port || 3000;
+const server = app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! ✴ Shutting down...');
+  server.close(() => process.exit(1));
+});
+
+process.on('SIGTERM', () => {
+  console.log('🔨 SIGTERM RECIEVED');
+  server.close(() => 'Process terminated!');
+});
