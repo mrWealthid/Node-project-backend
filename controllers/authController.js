6117bf3f3de87ffe4ef2bc38e263c71ee8d772b6
wealthiduwe <wealthiduwe@gmail.com>
2023-08-16 09:22:39 -0400

    update: Update error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -53,7 +53,12 @@ function generateUniqueAccountNumber() {
   return result;
 }
 
-exports.signup = catchAsync(async (req, res) => {
+exports.signup = catchAsync(async (req, res, next) => {
+  
+  const checkIfUserExist = await User.findOne({email: req.body.email})
+  if(checkIfUserExist)return next(new AppError('Email is already in use', 400));
+
+
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
@@ -72,8 +77,9 @@ exports.signup = catchAsync(async (req, res) => {
   // await new Email(newUser, url).sendMyMail()
 
   createSendToken(newUser, 201, req, res);
-  
-});
+}
+
+);
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
@@ -85,6 +91,10 @@ exports.login = catchAsync(async (req, res, next) => {
   const user = await User.findOne({
     email,
   }).select('+password');
+
+  // if(!user) {
+  //   return next(new AppError("User does not exit", 401));
+  // }
 
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError('Incorrect email or password', 401));
